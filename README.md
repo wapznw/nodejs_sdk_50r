560806b25bfff79c70c7aeaab88cee4b11940145
自娱自乐 <wapznw@gmail.com>
2016-08-12 11:32:31 +0800

    update readme,test

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 ### 50R.CN 短网址 node.js SDK
-
-[![NPM version][npm-image]][npm-url]
+---
+[![NPM version](https://img.shields.io/npm/v/url50r.svg?style=flat)](https://www.npmjs.org/package/url50r)
 [![Build Status](https://travis-ci.org/wapznw/nodejs_sdk_50r.svg?branch=master)](https://travis-ci.org/wapznw/nodejs_sdk_50r)
 [![Coverage Status](https://coveralls.io/repos/github/wapznw/nodejs_sdk_50r/badge.svg?branch=master)](https://coveralls.io/github/wapznw/nodejs_sdk_50r?branch=master)
 
 使用方法
 
 ```
-npm install url50r
+npm install url50r --save
 ```
 
 
--- test.js
+++ test.js
@@ -2,24 +2,36 @@
  * Created by wdj on 16/8/11.
  * test
  */
+var assert = require("assert");
 var url50r = require('./index');
+var it = require("mocha/lib/mocha.js").it;
+var describe = require("mocha/lib/mocha.js").describe;
 
 var ak = '57ac3816d9cb467d121dd48f';
 var longUrl = 'https://www.google.com.hk/search?q=hello';
 
-/* demo 1 */
+describe('test short url', function () {
+    it(longUrl, function (done) {
+        url50r.setAppKey(ak).convert(longUrl).then(function (result) {
+            assert.equal('http://50r.cn/YM6qRM', result);
+            done();
+        }).catch(done);
+    })
+});
+
+/*/!* demo 1 *!/
 url50r.setAppKey(ak).convert(longUrl).then(function (result) {
     console.log(result);
 }).catch(function (err) {
     console.error(err);
 });
-/* demo 1 end*/
+/!* demo 1 end*!/
 
-/* demo 2 */
+/!* demo 2 *!/
 url50r.setAppKey(ak);
 url50r.convert(longUrl).then(function (shortUrl) {
     console.log(shortUrl);
 }).catch(function (err) {
     console.error(err);
 });
-/* demo 2 end*/
+/!* demo 2 end*!/*/
