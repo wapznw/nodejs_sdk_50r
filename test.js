ff215a11dc6a3f684ba6672086c73c9db9927d9f
自娱自乐 <wapznw@gmail.com>
2016-08-12 12:17:05 +0800

    update readme,test

--- test.js
+++ test.js
@@ -13,6 +13,7 @@ var describe = require("mocha/lib/mocha.js").describe;
 var longUrl = 'https://www.google.com.hk/search?q=hello';
 describe('test short url', function () {
     it(longUrl, function (done) {
+        this.timeout(30000);
         url50r.convert(longUrl).then(function (result) {
             assert.equal('YM6qRM', String(result).substr(-6));
             done();
